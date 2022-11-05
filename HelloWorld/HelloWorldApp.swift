3f6360faeaaacadb4df8caf71c657a130d55cf78
Jakub Gania <contact@jakubgania.io>
2022-11-05 16:07:50 +0100

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Jakub Gania on 05.11.22.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
